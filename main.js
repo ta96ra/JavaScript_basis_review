e2d07de88033b092185977bbb20f89535956adc9
ta96ra <ta9mura4ge10@gmail.com>
2023-02-22 17:46:35 +0900

    [Update]

--- main.js
+++ main.js
@@ -55,9 +55,9 @@
   const total = sum(1,2,3) + sum(4,5,6);
   console.log(total);
 }
-// -------------
-// オブジェクト
-// -------------
+// --------------------
+// オブジェクト（配列）
+// --------------------
 {
   // 配列、呼出、変更、length
   const scores = [80,90,40];
@@ -134,6 +134,192 @@
 // -----------------------
 
 {
+  // オブジェクトの定義、呼出、プロパティーの追加、削除
+  const point = {
+    x: 100,
+    y: 180
+  }
+  console.log(point.x);
+  console.log(point['y']);
+  point.z = 90; //z:90の追加
+  delete point.y; //yの削除
+  console.log(point);
+}
+{
+  // スプレット構文でオブジェクトにオブジェクトの複数プロパティーを追加
+  const otherPops = {
+    r: 4,
+    color: 'red'
+  }
+  const point = {
+    x: 100,
+    y: 180,
+    ...otherPops
+  }
+  console.log(point);  //{x:100,y:180,r:4,color:'red'}
+
+  // 分割代入を使ってオブジェクトの値の取り出し
+  const {x,r,...others} = point;
+  console.log(r);
+  console.log(others)
+
+  // Object.keys()を使ってキーのみ取り出し
+  const keys = Object.keys(point);
+  console.log(keys);  //['x','y','r','color']
+  keys.forEach(key=>{
+    console.log(`${key}:${point[key]}`);
+  });
+
+  // 配列の中にオブジェクトが入っている時のデータ取得
+  const points = [
+    {x:30,y:20},
+    {x:10,y:50},
+    {x:40,y:40}
+  ];
+  // y:50を取得したいとき
+  console.log(points[1].y);
+}
+{
+  // 配列のコピーについて
+  let x = [1,2];
+  // let y = x;  //このように書くと丸ごと紐付けられる
+  // x[0] = 5;
+  // console.log(x); //[5,2]
+  // console.log(y); //[5,2]
+
+  let y = [...x];  //スプレッド演算子mamp で書くと値そのもを直接代入される
+  x[0] = 5;
+  console.log(x); //[5,2]
+  console.log(y); //[1,2]
+}
+
+// -------------------
+// 文字列の操作
+// -------------------
+{
+  // length,substring
+  const str = 'hello';
+  console.log(str.length); //文字数 5
+  console.log(str.substring(2,4));  //index番号2から4番手前の文字列を切り取る ll
+  console.log(str[1]);  //index番号1の文字列 e
+  
+  // join():配列を文字列に結合
+  const d = [2023,2,22];
+  console.log(d.join('/')); //2023/2/22
+
+  // split():文字列を配列に分割
+  const t = '12:42:35';
+  console.log(t.split(':')); //['12','42','35']
+  console.log(t.split(''));  //['1','2',':','4','2',':','3','5']
+
+  // 分割代入で取り出し
+  const[hour,minute,second] = t.split(':');
+  console.log(minute);   //42
+}
+
+// --------------------
+// 数値の操作
+// --------------------
+{
+// Math.floor(),Mathceil(),Math.round(),toFixed()
+  const avg = 7.6666666666
+  console.log(Math.floor(avg)); //整数で切り下げ
+  console.log(Math.ceil(avg));  //整数で切り上げ
+  console.log(Math.round(avg));  //整数で四捨五入
+  console.log(avg.toFixed(3));  //小数点3桁で四捨五入 7.667
+
+  //Math.randam()
+  console.log(Math.random()); //0~1の間で乱数の生成
+  console.log(Math.ceil(Math.random() * 6)); //サイコロ
+}
+// --------------
+// 日時
+// --------------
+{
+  // 現在日時
+  const d = new Date();  //現在地における現在日時
+  console.log(d);
+
+  console.log(d.getHours());  //時の取得
+  console.log(d.getSeconds());  //分の取得
+  // console.log(d.getMillseconds()); //??
+  console.log(d.getMonth());  //月の取得(0:1月~)
+  console.log(d.getDate());  //日の取得
+  console.log(d.getDay());  //曜日の取得(0：日,1:月~)
+  console.log(d.getFullYear());  //年の取得
+  // const n = getTime();  //??
+  // console.log(n);
+}
+{
+  // 特定日時
+  const d = new Date(2000,11);  //2000年12月に指定
+  console.log(d);
+
+  // d.setHours(10);
+  d.setHours(10,20,30); //10時20分30秒に指定
+  d.setDate(31);  //31日に指定
+  console.log(d);  //2000年12月31日10時20分30秒
+}
+
+//------------------
+// 様々な機能
+//-------------------
+
+// ----- alert()とconfirm() --------
+{
+  alert('hello');
+  const answer = confirm('削除しますか？');
+  if(answer){
+    console.log('削除しました');//OKを押すとtrueを返す
+  }else{
+      console.log('キャンセルしました');//キャンセルを押すとfalseを返す
+  }
+
+  // --------- setInterval()とclearInterbal() ---------
+
+  //setInterval(a,b); bミリ秒おきにaを無限に実行
+  //clearInterval(c); cのsetIntervalを解除
+
+  let i = 0
+  function showTime(){
+    console.log(new Date());
+    i++;
+    if(i > 2){
+      clearInterval(intervalId);
+    }
+  }
+  const intervalId = setInterval(showTime,1000);
+  // 現在時を1sごとに更新し、i=0からi=2になったら処理の終了
+  // 現在時が1sごとに３回表示される
 
 
+  // ---------- setTimeout()とclearTimeout() ------------
+
+  //setTimeout(a,b); bミリ秒おきに1回だけaを実行
+  //clearTimeout(c); cという繰り返されたsetTimeoutを解除
+
+  // 3回繰り返す方法
+  let n = 0;
+  function showTimeout(){
+    console.log(new Date());
+    const timeoutId = setTimeout(showTimeout,1000);
+    n++;
+    if(n > 2){
+      clearTimeout(timeoutId);
+    }
+  }
+  showTimeout();
+}
+
+{
+  // ---------- オブジェクトに関数を入れてみる ---------
+  const posts = [
+    {
+      text: 'JavaScriptの勉強中',
+      likeCount: 0,
+      Show: function show(post){
+        console.log(`${post.text} - ${post.likeCount}いいね`)
+      },
+    }
+  ];
 }
